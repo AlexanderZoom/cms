fda8fd8c16807b347530457f8936c7c1973947ce
AlexanderZoom <geroyster@gmail.com>
2025-08-09 02:41:45 +0300

    ffff

--- application/config/main.php
+++ application/config/main.php
@@ -1,9 +1,9 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 return array (
   'cookie_salt' => 'fj83flsa',
-  'logs' => 'E:\workspaces\php-wamp\cms.local\application\data\logs',
-  'cron_logs' => 'E:\workspaces\php-wamp\cms.local\application\data\cron\logs',
-  'cron_pid' => 'E:\workspaces\php-wamp\cms.local\application\data\cron/pid',
-  'cron_time' => 'E:\workspaces\php-wamp\cms.local\application\data\cron\time',
-  'data_dir' => 'E:\workspaces\php-wamp\cms.local\application\data',
+  'logs' => 'F:\workspaces\php-wamp\cms\application\data\logs',
+  'cron_logs' => 'F:\workspaces\php-wamp\cms\application\data\cron\logs',
+  'cron_pid' => 'F:\workspaces\php-wamp\cms\application\data\cron/pid',
+  'cron_time' => 'F:\workspaces\php-wamp\cms\application\data\cron\time',
+  'data_dir' => 'F:\workspaces\php-wamp\cms\application\data',
 );
